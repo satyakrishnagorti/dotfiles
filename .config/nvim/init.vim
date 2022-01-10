49fb7fb5d5f0a74bc299d8c4fb68c851d87add8b
Satya Krishna Gorti <satyakrishna.gorti@gmail.com>
2022-01-10 00:01:49 -0500

    lua port nvim 0.5

---
+++ .config/nvim/init.vim
@@ -0,0 +1,163 @@
+syntax on
+
+set noerrorbells
+set tabstop=4 softtabstop=4
+set shiftwidth=4
+set expandtab
+set smartindent
+set nu
+set nowrap
+set smartcase
+set incsearch
+set relativenumber
+set hidden
+set noshowmatch
+set background=dark
+set mouse=a
+set guifont=Literation\ Mono\ Powerline:h20
+set scrolloff=8  " start scrolling if you are lines away from end of page
+set signcolumn=yes
+set colorcolumn=120
+
+" highlight all on search and no highlight on ESC
+augroup vimrc-incsearch-highlight
+  autocmd!
+  autocmd CmdlineEnter /,\? :set hlsearch
+  autocmd CmdlineLeave /,\? :set nohlsearch
+augroup END
+
+
+call plug#begin('~/.vim/plugged')
+    " Common plugins
+    Plug 'tpope/vim-fugitive'
+    Plug 'vim-utils/vim-man'
+    Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
+    Plug 'junegunn/fzf.vim'
+    Plug 'tpope/vim-commentary'
+    Plug 'scrooloose/nerdtree'
+    Plug 'Xuyuanp/nerdtree-git-plugin'
+    Plug 'tpope/vim-surround'
+    " Plug 'dense-analysis/ale'
+    Plug 'mhinz/vim-signify'
+    " Themes
+    Plug 'morhetz/gruvbox'
+    Plug 'sonph/onehalf', { 'rtp': 'vim' }
+    Plug 'joshdick/onedark.vim'
+    Plug 'dracula/vim', { 'as': 'dracula' }
+    Plug 'arzg/vim-colors-xcode'
+    Plug 'EdenEast/nightfox.nvim'
+
+    " Plug 'numirias/semshi', {'do': ':UpdateRemotePlugins'}
+
+    " Telescope
+    Plug 'nvim-lua/plenary.nvim'
+    Plug 'nvim-telescope/telescope.nvim'
+
+    " Treesitter
+    Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+    Plug 'nvim-treesitter/nvim-treesitter-textobjects'
+    Plug 'nvim-treesitter/nvim-treesitter-refactor'
+
+    " LSP
+    Plug 'neovim/nvim-lspconfig'
+    Plug 'kabouzeid/nvim-lspinstall'
+    Plug 'hrsh7th/cmp-nvim-lsp'
+    Plug 'hrsh7th/cmp-buffer'
+    Plug 'hrsh7th/nvim-cmp'
+    Plug 'glepnir/lspsaga.nvim'
+    Plug 'onsails/lspkind-nvim'
+    Plug 'ray-x/lsp_signature.nvim'
+    Plug 'nvim-lua/lsp-status.nvim'
+    Plug 'simrat39/symbols-outline.nvim'
+
+    " Statusline
+    Plug 'nvim-lualine/lualine.nvim'
+
+    " Icons
+    Plug 'kyazdani42/nvim-web-devicons'
+
+    " Rust
+    Plug 'rust-lang/rust.vim'
+    Plug 'simrat39/rust-tools.nvim'
+
+    " Debugging
+    Plug 'mfussenegger/nvim-dap'
+
+
+call plug#end()
+
+let mapleader = " "
+
+" colorscheme related
+" colorscheme dracula
+colorscheme duskfox
+" let g:gruvbox_contrast_dark = 'medium'
+" let g:airline_theme='onehalfdark'
+
+
+inoremap jj <Esc>
+map <C-t> :NERDTreeToggle<CR>
+
+nnoremap <leader>h :wincmd h<CR>
+nnoremap <leader>j :wincmd j<CR>
+nnoremap <leader>k :wincmd k<CR>
+nnoremap <leader>l :wincmd l<CR>
+
+" setting when attached to detected lsp: see lua/lsp.lua
+" nnoremap <silent> gd    <cmd>lua vim.lsp.buf.definition()<CR>
+" nnoremap <silent> <C-]> <cmd>lua vim.lsp.buf.definition()<CR>
+" nnoremap <silent> gD    <cmd>lua vim.lsp.buf.declaration()<CR>
+" nnoremap <silent> gr    <cmd>lua vim.lsp.buf.references()<CR>
+" nnoremap <silent> gi    <cmd>lua vim.lsp.buf.implementation()<CR>
+" nnoremap <silent> <C-k> <cmd>lua vim.lsp.buf.signature_help()<CR>
+" nnoremap <silent> gf    <cmd>lua vim.lsp.buf.formatting()<CR>
+" nnoremap <silent> gn    <cmd>lua vim.lsp.buf.rename()<CR>
+
+" Not using Lspsaga for now as the maintained branch is 
+" suported for 0.6 nightly
+" Need to change to this when the project gets better
+" https://github.com/tami5/lspsaga.nvim
+" nnoremap <silent> ga    <cmd>Lspsaga code_action<CR>
+" xnoremap <silent> ga    <cmd>Lspsaga range_code_action<CR>
+" nnoremap <silent> gs    <cmd>Lspsaga signature_help<CR>
+" nnoremap <silent><space>rn  <cmd>lua require('lspsaga.rename').rename()<CR>
+" nnoremap <silent> <C-p> <cmd>Lspsaga diagnostic_jump_prev<CR>
+" nnoremap <silent> <C-n> <cmd>Lspsaga diagnostic_jump_next<CR>
+" nnoremap <silent> K     <cmd>Lspsaga hover_doc<CR>
+
+
+nnoremap <leader>ff :GFiles<CR>
+nnoremap <Leader><space> :Files<CR>
+nnoremap <Leader>bb :Buffers<CR>
+nnoremap <silent> <leader>/ :Ag<CR>
+
+nnoremap ,f <cmd>Telescope find_files<cr>
+nnoremap ,g <cmd>Telescope live_grep<cr>
+nnoremap ,b <cmd>Telescope buffers<cr>
+nnoremap ,h <cmd>Telescope help_tags<cr>
+nnoremap ,mp <cmd>Telescope man_pages<cr>
+nnoremap ,q <cmd>Telescope quickfix<cr>
+nnoremap ,r <cmd>Telescope  registers<cr>
+nnoremap ,n <cmd>Telescope file_browser<cr>
+
+nnoremap <C-p> :bprev<CR>
+nnoremap <C-n> :bnext<CR>
+
+" airline
+" let g:airline#extensions#tabline#enabled = 1
+" let g:airline#extensions#ale#enabled = 1
+
+" ale symbols
+" let g:ale_sign_error = '✘'
+" let g:ale_sign_warning = '⚠'
+" highlight ALEErrorSign ctermbg=NONE ctermfg=red
+" highlight ALEWarningSign ctermbg=NONE ctermfg=yellow
+
+
+
+lua <<EOF
+require("lsp")
+require('statusline')
+require('treesitter')
+EOF
+
---
+++ .config/nvim/lua/lsp.lua
@@ -0,0 +1,127 @@
+local cmp = require'cmp'
+local lspkind = require'lspkind'
+cmp.setup({
+  snippet = {
+    -- expand = function(args)
+    --   -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+    --   -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+    --   -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    --   -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
+    -- end,
+  },
+  mapping = {
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<C-y>'] = cmp.config.disable, -- If you want to remove the default `<C-y>` mapping, You can specify `cmp.config.disable` value.
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' })
+  },
+
+  experimental = {
+    ghost_text = true,
+  },
+  
+  documentation = {
+    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+  },
+
+  sources = cmp.config.sources(
+    {
+      { name = 'nvim_lsp' },
+      { name = "treesitter" },
+      { name = "path" },
+      -- { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, 
+    {
+      { name = 'buffer' },
+    }
+  ),
+
+
+  formatting = {
+    format = function(entry, vim_item)
+      vim_item.kind = string.format("%s %s", lspkind.presets.default[vim_item.kind], vim_item.kind)
+      vim_item.menu = ({
+        nvim_lsp = "ﲳ",
+        nvim_lua = "",
+        treesitter = "",
+        path = "ﱮ",
+        buffer = "﬘",
+        zsh = "",
+        vsnip = "",
+        spell = "暈",
+      })[entry.source.name]
+
+      return vim_item
+    end,
+  },
+})
+
+-- Setup lspconfig.
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+-- setting up language specific lsp configs
+-- require'lspconfig'.tsserver.setup{capabilities = capabilities}
+-- require'lspconfig'.rust_analyzer.setup{capabilities = capabilities}
+-- require'lspconfig'.pyright.setup{capabilities = capabilities}
+
+local nvim_lsp = require('lspconfig')
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  -- Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+
+  -- LSP Signature setup
+  require "lsp_signature".on_attach()
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+
+end
+
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local servers = { 'pyright', 'rust_analyzer', 'tsserver' }
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup {
+    on_attach = on_attach,
+    flags = {
+      debounce_text_changes = 150,
+    },
+    capabilities = capablities
+  }
+end
+
+-- Rust tools setup
+require('rust-tools').setup({})
+
+
